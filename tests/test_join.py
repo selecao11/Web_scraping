629d4d6fcacc6f42e0ce47255be6f09f3500f740
okihara <okihara6@gmail.com>
2023-08-26 19:36:27 +0900

    2023_0826_1936

--- tests/test_join.py
+++ tests/test_join.py
@@ -1,12 +1,16 @@
-from join import Join
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '../'))
+from web_scraping.join import Join
 import pandas as pd
+csv_path = "/home/user/anaconda3/envs/web_scraping/web_scraping/"
 
 def test_join_succes1():
-    join_kabuka_df = pd.read_csv('/home/user/anaconda3/envs/web_scraping/source/test/三菱自動車_株価_.csv')
-    join_shinyou_zan_df = pd.read_csv('/home/user/anaconda3/envs/web_scraping/source/test/三菱自動車_信用残_.csv')
-    join_gyakuhibu_taisyaku_df = pd.read_csv('/home/user/anaconda3/envs/web_scraping/source/test/三菱自動車_逆日歩_貸借桟_.csv')
+    join_kabuka_df = pd.read_csv(csv_path +'tests/三菱自動車_株価_.csv')
+    join_shinyou_zan_df = pd.read_csv(csv_path + 'tests/三菱自動車_信用残_.csv')
+    join_gyakuhibu_taisyaku_df = pd.read_csv(csv_path + 'tests/三菱自動車_逆日歩_貸借桟_.csv')
 
     jb = Join()
     jb.nikei_join_init(join_kabuka_df,join_shinyou_zan_df,join_gyakuhibu_taisyaku_df)
     nikei_join_df = jb.nikei_jion()
-    nikei_join_df.to_csv('/home/user/anaconda3/envs/web_scraping/source/test/三菱自動車_株価_信用残_逆日歩_貸借桟.csv')
+    nikei_join_df.to_csv(csv_path + 'tests/三菱自動車_株価_信用残_逆日歩_貸借桟.csv')
