c137f85943da455dcda2b7d35efa4be33f41d72f
okihara <okihara6@gmail.com>
2023-08-30 08:48:51 +0900

    2023_0830_0848

--- tests/test_link_niltukei.py
+++ tests/test_link_niltukei.py
@@ -6,6 +6,7 @@ from web_scraping.kabuka import Kabuka
 from web_scraping.gyakuhibu_taisyaku import Gyakuhibu_taisyaku
 from web_scraping.shinyou_zan import Shinyou_zan
 from web_scraping.join import Join
+from web_scraping.merge import Merge
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 import pandas as pd
@@ -27,32 +28,40 @@ def test_driver_get():
 def test_niltukei_succes1():
     driver = test_driver_get()
     kb = Kabuka()
-    kb_title = kb.kabuka_title_get(driver)
     file_path = './'
-    file_name = kb_title + '_link_株価.csv'
+    file_name = '_link_株価.csv'
+    kb.kabuka_title_get(driver)
+    kb.kabuka_taisyaku_init_set(file_name,file_path)
     kb.kabuka_df_cleate(WebDriverWait,driver,pd,By,file_path,file_name)
     print("ok")
 
     gt = Gyakuhibu_taisyaku()
-    gt.gyakuhibu_taisyaku_title_get(driver)
     file_path = './'
     file_name = '_link_逆日歩_貸借桟.csv'
+    gt.gyakuhibu_taisyaku_title_get(driver)
     gt.gyakuhibu_taisyaku_init_set(file_name,file_path)
     gt.gyakuhibu_taisyaku_df_cleate(WebDriverWait,driver,pd,By)
 
     sz = Shinyou_zan()
-    sz.shinyou_zan_title_get(driver)
     file_path = './'
     file_name = '_link_信用残.csv'
+    sz.shinyou_zan_title_get(driver)
     sz.shinyou_zan_init_set(file_name,file_path)
     sz.shinyou_zan_df_cleate(WebDriverWait,driver,pd,By)
 
     csv_path = "/home/user/anaconda3/envs/web_scraping/web_scraping/"
     join_kabuka_df = pd.read_csv(csv_path +'tests/三菱自動車_link_株価.csv')
-    join_shinyou_zan_df = pd.read_csv(csv_path + 'link_tests/三菱自動車_link_信用残.csv')
-    join_gyakuhibu_taisyaku_df = pd.read_csv(csv_path + 'link_tests/三菱自動車_link_逆日歩_貸借桟.csv')
+    join_shinyou_zan_df = pd.read_csv(csv_path + 'tests/三菱自動車_link_信用残.csv')
+    join_gyakuhibu_taisyaku_df = pd.read_csv(csv_path + 'tests/三菱自動車_link_逆日歩_貸借桟.csv')
 
     jb = Join()
     jb.nikei_join_init(join_kabuka_df,join_shinyou_zan_df,join_gyakuhibu_taisyaku_df)
     nikei_join_df = jb.nikei_jion()
     nikei_join_df.to_csv(csv_path + 'tests/三菱自動車_link_株価_信用残_逆日歩_貸借桟.csv')
+
+    ruiseki_df = pd.read_csv(csv_path + '/tests/三菱自動車_累積.csv')
+    nikei_data_df = pd.read_csv(csv_path + '/tests/三菱自動車_link_株価_信用残_逆日歩_貸借桟.csv')
+    mg = Merge()
+    mg.nikei_merge_init(ruiseki_df,nikei_data_df)
+    merge_df = mg.nikei_merge()
+    merge_df.to_csv(csv_path + '/tests/三菱自動車_link_マージ.csv')
