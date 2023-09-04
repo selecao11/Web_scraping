444bd58004e7a8af25ba4534d1bc68b5630e8baf
okihara <okihara6@gmail.com>
2023-09-04 10:40:43 +0900

    2023_0904_1040

--- web_scraping/shinyou_zan_df_comp_test.py
+++ web_scraping/shinyou_zan_df_comp_test.py
@@ -5,25 +5,21 @@ def shinyou_zan_df_comp():
         gyakuhibu_taisyaku_df = pd.read_csv(csv_path +'スノーピーク_test_逆日歩_貸借桟.csv')
         ruiseki_df = pd.read_csv(csv_path + 'スノーピーク_累積.csv')
         ruiseki_df["日付"]=ruiseki_df["日付"].astype("datetime64[ns]")
-        ruiseki_df = ruiseki_df[~ruiseki_df['累積貸株残'].isin(gyakuhibu_taisyaku_df['貸株残'])]
-        dd = ruiseki_df["日付"][1].strftime('%Y-%m-%d')
-        #iterrow()
-        #dd = ruiseki_df.loc[1,"日付"].
+        #逆日歩_貸株残と累積でデータ誤差の有無のチェック
+        ruiseki_Non_stock_lending_df = ruiseki_df[~ruiseki_df['累積貸株残'].isin(gyakuhibu_taisyaku_df['貸株残'])]
+        #累積のデータ誤差発生日を抽出
+        bbb=list()
+        bbb.append(ruiseki_Non_stock_lending_df.loc[1,"日付"].strftime('%Y-%m-%d'))
+        print(bbb)
+        #逆日歩_貸借桟の該当日データを抽出
+        aaa = gyakuhibu_taisyaku_df[gyakuhibu_taisyaku_df['日付'].isin(bbb)]
+        #逆日歩_貸借桟の貸株残を抽出
+        ccc = aaa.loc[:,'貸株残'].values
+        print(ccc)
+        print(bbb[0])
+        ruiseki_df.loc[ruiseki_df['日付']==bbb[0],'累積貸株残']=0
+        #逆日歩_貸株残の日付で累積データを抽出
 
-        print(dd)
-
-        #ruiseki_df.loc[ruiseki_df[["日付"] == gyakuhibu_taisyaku_df["日付"]],gyakuhibu_taisyaku_df['貸株残']]
         ruiseki_df.to_csv(csv_path + 'shinyou_zan_df_comp.csv')
-        '''
-        shinyou_zan_merge_df = pd.merge(self.ruiseki_df,self.shinyou_zan_df, on=["日付",],how='outer',indicator=True)
-        shinyou_zan_df = shinyou_zan_merge_df[shinyou_zan_merge_df["_merge"] == "both"]
-        shinyou_zan_df=(shinyou_zan_df["累積信用売残"]==shinyou_zan_df["信用売残"])
-        file_name="_both.csv"
-        print('--累積信用売残==信用売残 start--')
-        print(shinyou_zan_df)
-        print('--累積信用売残==信用売残 end--')
-        shinyou_zan_df.to_csv(self.shinyou_zan_path + self.shinyou_zan_sz_title + file_name)
-        '''
-        pass
 
 shinyou_zan_df_comp()
